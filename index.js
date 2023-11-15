32991de26d20f65eba974e9b3ea718d2767b4139
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-11-14 17:17:21 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { JSDOM } from 'jsdom'
+
+JSDOM.fromFile("template.html", {
+  url: 'http://localhost',
+  runScripts: 'dangerously',
+  resources: 'usable',
+  pretendToBeVisual: true,
+}).then((dom) => {
+  console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+});
