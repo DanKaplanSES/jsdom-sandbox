939503232cda6be78392237cf1d28c67d1340477
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-12-04 17:33:42 -0800

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+/* eslint-disable no-unused-vars */
 import { JSDOM } from 'jsdom';
 
-JSDOM.fromFile('template.html', {
+console.log(`globalThis.module`, globalThis.module);
+console.log(`global.module`, global.module);
+// console.log(`window.module`, window.module);
+console.log(`module`, module);
+
+console.log(`start`);
+
+const dom = await JSDOM.fromFile('template1.html', {
   url: 'http://localhost',
   runScripts: 'dangerously',
   resources: 'usable',
   pretendToBeVisual: true,
-}).then((dom) => {
-  console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
 });
+const window = dom.window;
+const document = window.document;
+
+console.log('p textContent vanilla', document.querySelector('p').textContent); // "Hello world"
+
+globalThis.window = window;
+globalThis.document = document;
+
+const previousModule = globalThis.module;
+console.log(previousModule);
+globalThis.module = undefined;
+
+
+const dynamicImport = await import('jquery');
+// console.log(`1`, dynamicImport);
+// console.log(`2`, dynamicImport.default);
+// console.log(`3`, typeof dynamicImport.default);
+// console.log(`4`, dynamicImport.default());
+// console.dir(dynamicImport.default);
+
+const $ = dynamicImport.default;
+// console.dir($);
+
+console.log(`html`, $('body').html());
+
+console.log('p textContent jQuery', $('p').textContent); // "Hello world'
+
