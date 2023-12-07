f63619a6f9e08a80d58d2e0cfc6c820787a9ddf6
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-12-06 19:48:07 -0800

    converted index.js into a spec file

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,42 @@
+import { JSDOM } from 'jsdom';
+
 describe('jquery', () => {
-  it('uses the currently available document', () => {
-    expect(true).toBe(false);
+  it('uses the currently available document', async () => {
+    const { document: template1Document, jquery: template1Jquery } =
+      await parseHtml('template1.html');
+    expect(template1Document.querySelector('p').textContent).toEqual(
+      'Hello world'
+    );
+    expect(template1Jquery.find('p').text()).toEqual('Hello world');
+
+    const { document: template2Document, jquery: template2Jquery } =
+      await parseHtml('template2.html');
+    expect(template2Document.querySelector('p').textContent).toEqual(
+      'Goodbye world'
+    );
+    expect(template2Jquery.find('p').text()).toEqual('Goodbye world');
   });
 });
+
+async function parseHtml(fileName) {
+  const dom = await JSDOM.fromFile(fileName, {
+    url: 'http://localhost',
+    runScripts: 'dangerously',
+    resources: 'usable',
+    pretendToBeVisual: true,
+  });
+  const window = dom.window;
+  const document = window.document;
+
+  globalThis.window = window;
+  globalThis.document = document;
+
+  const dynamicImport = await import('jquery');
+
+  const $ = dynamicImport.default;
+
+  return {
+    document: document,
+    jquery: $(`html`),
+  };
+}
