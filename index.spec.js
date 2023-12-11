32bc44adb7a67aeea9c3a58bf6db4f3e853427f1
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-12-11 15:39:59 -0800

    added a new test case

--- index.spec.js
+++ index.spec.js
@@ -8,9 +8,15 @@ describe('jquery', () => {
     const dynamicImport = await import('jquery');
     const $ = dynamicImport.default(dom.window);
     */
-    await parseHtml('template1.html');
-    
-    const { document: template1Document, jquery: template1Jquery } = await parseHtmlWithJQueryStaticImport('template1.html');
+   await parseHtml('template1.html');
+   
+    /*
+    This will run:
+    import jQuery from 'jquery';
+    return { document: dom.window.document, jquery: jQuery(`html`), };
+    */
+    const { document: template1Document, jquery: template1Jquery } = await parseHtmlWithJQueryStaticImport('template1.html'); // !!!
+    // Error: jQuery requires a window with a document
     expect(template1Document.querySelector('p').textContent).toEqual( 'Hello world' );
     expect(template1Jquery.find('p').text()).toEqual('Hello world');
 
@@ -20,6 +26,12 @@ describe('jquery', () => {
     const $ = dynamicImport.default(dom.window);
     */
     await parseHtml('template2.html');
+
+    /*
+    This will run:
+    import jQuery from 'jquery';
+    return { document: dom.window.document, jquery: jQuery(`html`), };
+    */
     const { document: template2Document, jquery: template2Jquery } = await parseHtmlWithJQueryStaticImport('template2.html');
     expect(template2Document.querySelector('p').textContent).toEqual( 'Goodbye world' );
     expect(template2Jquery.find('p').text()).toEqual('Goodbye world');
