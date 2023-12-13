732d9cda1777f80d0bc3efb0ee3f5684174df28b
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-12-13 14:12:30 -0800

    failing "test case"

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import { JSDOM } from 'jsdom';
+import chalk from 'chalk';
+import * as Diff from 'diff';
 
-JSDOM.fromFile('template.html', {
-  url: 'http://localhost',
-  runScripts: 'dangerously',
-  resources: 'usable',
-  pretendToBeVisual: true,
-}).then((dom) => {
-  console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
+const one = 'beep boop ';
+const other = '\nbeep boob blah\t';
+
+const diff = Diff.diffChars(one, other);
+
+diff.forEach((part, index) => {
+  // green for additions, red for deletions
+  // grey for common parts
+  const color = part.added
+    ? chalk.bgGreen
+    : part.removed
+      ? chalk.bgRed
+      : (x) => x; // identity function, return with normal color
+  const coloredText = color(part.value);
+  process.stderr.write(coloredText);
 });
+
+console.log();
