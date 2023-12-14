30eca0cbd0c676f621f7db32e9b7b865558fb171
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2023-12-14 12:51:04 -0800

    split test into two.

--- index.spec.js
+++ index.spec.js
@@ -1,39 +1,28 @@
-import { parseHtmlWithDynamicImport } from './test-setup.js';
-import { parseHtmlWithJQueryStaticImport } from './prod-code.js';
+import * as testSetup from './test-setup.js';
+import * as prodCode from './prod-code.js';
 
 describe('jquery', () => {
-  it('affects later static imports', async () => {
-    /*
-    This will run:
-    const dynamicImport = await import('jquery');
-    const $ = dynamicImport.default(dom.window);
-    */
-   await parseHtmlWithDynamicImport('template1.html');
-   
-    /*
-    This will run:
-    import jQuery from 'jquery';
-    return { document: dom.window.document, jquery: jQuery(`html`), };
-    */
-    const { document: template1Document, jquery: template1Jquery } = await parseHtmlWithJQueryStaticImport('template1.html'); // !!!
+  it('dynamic imports affect static imports template1', async () => {
+    await testSetup.parseHtmlWithDynamicImport('template1.html');
+
+    const { document: template1Document, jquery: template1Jquery } =
+      await prodCode.parseHtmlWithJQueryStaticImport('template1.html'); // !!!
     // Error: jQuery requires a window with a document
-    expect(template1Document.querySelector('p').textContent).toEqual( 'Hello world' );
+    expect(template1Document.querySelector('p').textContent).toEqual(
+      'Hello world'
+    );
     expect(template1Jquery.find('p').text()).toEqual('Hello world');
+  });
 
-    /*
-    This will run:
-    const dynamicImport = await import('jquery');
-    const $ = dynamicImport.default(dom.window);
-    */
-    await parseHtmlWithDynamicImport('template2.html');
+  it('dynamic imports affect static imports template2', async () => {
+    await testSetup.parseHtmlWithDynamicImport('template2.html');
 
-    /*
-    This will run:
-    import jQuery from 'jquery';
-    return { document: dom.window.document, jquery: jQuery(`html`), };
-    */
-    const { document: template2Document, jquery: template2Jquery } = await parseHtmlWithJQueryStaticImport('template2.html');
-    expect(template2Document.querySelector('p').textContent).toEqual( 'Goodbye world' );
+    const { document: template2Document, jquery: template2Jquery } =
+      await prodCode.parseHtmlWithJQueryStaticImport('template2.html'); // !!!
+    // Error: jQuery requires a window with a document
+    expect(template2Document.querySelector('p').textContent).toEqual(
+      'Goodbye world'
+    );
     expect(template2Jquery.find('p').text()).toEqual('Goodbye world');
   });
 });
