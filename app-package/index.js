62a08429fe4fd321f2e0cc749814195dda0039a6
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-16 18:07:10 -0800

    modified package.json to work

--- app-package/index.js
+++ app-package/index.js
@@ -1,6 +1,6 @@
 import { ConfigurableResourceLoader } from 'jsdom-configurable-resource-loader';
 import { ResourceLoader } from 'jsdom';
-import { myFromFile } from 'lib-package';
+import { myFromFile } from 'dankaplanses-lib-package';
 
 console.log(`Is ConfigurableResourceLoader an instance of ResourceLoader?`, ConfigurableResourceLoader instanceof ResourceLoader);
 myFromFile(new ConfigurableResourceLoader());
