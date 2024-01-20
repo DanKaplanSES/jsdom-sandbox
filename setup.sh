0766d7bb5916a3e5bd9e9ec9b4b2e016ae63675b
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-19 21:08:05 -0800

    for convenience, remove package-lock.json files each time

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 #! /bin/bash
 set -e
 
+echo "#########################################"
+echo "#   lib-package: rm package-lock.json   #"
+echo "#########################################"
+rm lib-package/package-lock.json || true
+
 echo "################################"
 echo "#   lib-package: npm install   #"
 echo "################################"
 ./npml.sh install
+
+echo "#########################################"
+echo "#   app-package: rm package-lock.json   #"
+echo "#########################################"
+rm app-package/package-lock.json || true
+
+
 echo "################################"
 echo "#   app-package: npm install   #"
 echo "################################"
 ./npma.sh install
+
