602a7829c5815c052bfc670b3660468b1502e787
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-01-22 16:38:56 -0800

    Copying code from lib-has-peer-dependency-and-lib-is-file-dependency-so

--- app-package/index.js
+++ app-package/index.js
@@ -1,3 +1,6 @@
-import { libFun } from 'lib-package';
+import { ConfigurableResourceLoader } from 'jsdom-configurable-resource-loader';
+import { ResourceLoader } from 'jsdom';
+import { myFromFile } from 'lib-package';
 
-libFun();
+console.log(`Is ConfigurableResourceLoader an instance of ResourceLoader?`, ConfigurableResourceLoader instanceof ResourceLoader);
+myFromFile(new ConfigurableResourceLoader());
--- lib-package/index.js
+++ lib-package/index.js
@@ -1,3 +1,12 @@
-export function libFun() {
-  console.log('Version 1');
+import { JSDOM } from 'jsdom';
+
+export function myFromFile(resources) {
+  JSDOM.fromFile('template.html', {
+    url: 'http://localhost',
+    runScripts: 'dangerously',
+    resources,
+    pretendToBeVisual: true,
+  }).then((dom) => {
+    console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
+  });
 }
