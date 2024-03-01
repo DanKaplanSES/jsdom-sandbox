4dba7b018c45aa4a64a95b8e2f6939d1be9a9fd5
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-02-29 16:54:59 -0800

    Errors with "NodeList" not defined

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import { JSDOM } from 'jsdom';
 
 JSDOM.fromFile('template.html', {
-  url: 'http://localhost',
-  runScripts: 'dangerously',
-  resources: 'usable',
-  pretendToBeVisual: true,
+    url: 'http://localhost',
+    runScripts: 'dangerously',
+    resources: 'usable',
+    pretendToBeVisual: true,
 }).then((dom) => {
-  console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
+    const KEYS = [];
+    KEYS.push(
+        ...Object.getOwnPropertyNames(dom.window).filter(
+            (k) => {
+                console.log(k);
+                return !k.startsWith('_') && !(k in globalThis)
+            }
+        )
+    );
+
+    KEYS.forEach((key) => (globalThis[key] = dom.window[key]));
+
+    const indirectEval = eval;
+    indirectEval(`console.log(NodeList);`);
 });
