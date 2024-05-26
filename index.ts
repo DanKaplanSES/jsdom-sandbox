d50c9fee6fe8d699fe09ed312d46ccb10b80a265
Daniel Kaplan <daniel.kaplan@SleepEasySoftware.com>
2024-05-26 14:40:02 -0700

    thorough test case

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-import winston from "winston";
+import winston from 'winston';
 
 const logger = winston.createLogger({
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: 'combined.log' })
-    ]
-  });
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
-logger.error(`I meant to log this!!!`);
+logger.error(`logger: I meant to log this!!!`);
 
 const childLogger = logger.child({ requestId: '451', foo: 'bar' });
 
-childLogger.error(`I meant to log this!!!`);
+childLogger.error(`childLogger: I meant to log this!!!`);
+
+const grandchildLogger = childLogger.child({ baz: 'qux' });
+
+grandchildLogger.error(`grandchildLogger: I meant to log this!!!`);
